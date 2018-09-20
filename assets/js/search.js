c7836e4638a47771b20bf8ad32c49d114a16c04a
Will Son <willson@robotis.com>
2018-09-20 18:02:06 +0900

    refined search result

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
 
   $("#search-form").submit(function(event) {
     search_item = $("#search_term").val();
-    console.log("Form submitted");
-    console.log(search_item);
+    // console.log(search_item);
     if(search_item == '') {
       alert("Please enter the search term");
     }
@@ -60,32 +59,40 @@ function emanual_search(search_item) {
     
     var results = index.search(search_item, {
         fields: {
-            content: {}
+            product: {boost: 10},
+            header1: {boost: 4},
+            header2: {boost: 3},
+            header3: {boost: 2},
+            content: {boost: 1},
         }
     });
     
-    console.log(results.length);  
+    // console.log(results.length);  
     if (results.length > 0) {
       var i = 0;
-      search_result_contents += '<i class="fa fa-times fa-2x" aria-hidden="true" onclick=toggle_search_window()></i>' + '<br>' + '<ul style=text-align:left;>';
+      search_result_contents += '<i class="fa fa-times fa-2x" aria-hidden="true" onclick=toggle_search_window() style="position:fixed;"></i>' + '<br>' + '<ul style=text-align:left;>';
       for (i = 0; i < results.length; i++) {
         var result_doc = index.documentStore.getDoc(results[i].ref);
-        console.log(result_doc.url);
+        var result_score = results[i].score;
+        var result_content = jQuery.trim(result_doc.content).substring(0, 200).split(" ").slice(0, -1).join(" ") + "...";
+        // console.log(results);
         
-        search_result_contents += '<li>' + '<a href=' + result_doc.url + '>' + result_doc.product + " > " + result_doc.header1;
+        search_result_contents += '<li>' + "Score : " + result_score + '<br>';
+        search_result_contents += '<a href="' + result_doc.url + '" onclick=toggle_search_window()>' + result_doc.product + " / " + result_doc.header1;
         if (result_doc.header2 != '') {
-          search_result_contents += " > " + result_doc.header2;
+          search_result_contents += " / " + result_doc.header2;
         }
         if (result_doc.header3 != '') {
-          search_result_contents += " > " + result_doc.header3;
+          search_result_contents += " / " + result_doc.header3;
         }
         search_result_contents += '</a></li>';
+        search_result_contents += result_content + '<br><br>';
       }
       search_result_contents += '</ul>'
     }
     else {
-      search_result_contents += '<i class="fa fa-times fa-2x" aria-hidden="true" onclick=toggle_search_window()></i>' + '<br><br>' + '<div style="color:red; font-weight:600; font-size:1.5em; text-align:left;">' + "No Results Found with \"" + search_item + "\"" + '</div>';
-      console.log("No Results Found.");
+      search_result_contents += '<i class="fa fa-times fa-2x" aria-hidden="true" onclick=toggle_search_window() style="position:fixed;"></i>' + '<br><br>' + '<div style="color:red; font-weight:600; font-size:1.5em; text-align:left;">' + "No Results Found with \"" + search_item + "\"" + '</div>';
+      // console.log("No Results Found.");
     }
     toggle_search_window();
     $('.search-result').html(search_result_contents);
